da4344a39a8140c43c1114a048bf9b9281d0c82b
PierreRoussel <pierre.roussel01@free.fr>
2024-06-11 16:13:07 +0200

    Fix uniname

--- src/components/layout/Avatar.tsx
+++ src/components/layout/Avatar.tsx
@@ -3,6 +3,9 @@ import './Avatar.scss';
 export default function Avatar(params: { chain?: string; children?: any }) {
   const initials =
     params.chain &&
-    params.chain.split(' ')[0][0] + params.chain.split(' ')[1][0];
+    (params.chain.split(' ').length > 1
+      ? params.chain.split(' ')[0][0] + params.chain.split(' ')[1][0]
+      : params.chain.split(' ')[0][0]);
+
   return <div className='avatar'>{params.children || initials}</div>;
 }
