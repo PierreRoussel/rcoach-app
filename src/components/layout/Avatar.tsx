921d4386bd810c79c1acef9c5c007dedba401c7f
PierreRoussel <pierre.roussel01@free.fr>
2024-07-10 23:05:40 +0200

    seance du jour OK

--- src/components/layout/Avatar.tsx
+++ src/components/layout/Avatar.tsx
@@ -1,11 +1,25 @@
 import './Avatar.scss';
 
-export default function Avatar(params: { chain?: string; children?: any }) {
+export default function Avatar(params: {
+  chain?: string;
+  children?: any;
+  couleur?: string;
+}) {
   const initials =
     params.chain &&
     (params.chain.split(' ').length > 1
       ? params.chain.split(' ')[0][0] + params.chain.split(' ')[1][0]
       : params.chain.split(' ')[0][0]);
 
-  return <div className='avatar'>{params.children || initials}</div>;
+  return (
+    <div className='avatar'>
+      <span>{params.children || initials}</span>
+      <div
+        className='avatar-background'
+        style={{
+          ...(params.couleur && { background: params.couleur }),
+        }}
+      ></div>
+    </div>
+  );
 }
--- src/pages/seances/Seances.tsx
+++ src/pages/seances/Seances.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { IonContent, IonHeader, IonPage } from '@ionic/react';
 import { Link } from 'react-router-dom';
 
-import { supabase } from '../../services/supabaseClient';
 import PageHeaderIllu from '../../components/layout/PageHeaderIllu';
 import Bento from '../../components/layout/Bento';
 import Avatar from '../../components/layout/Avatar';
@@ -13,6 +12,8 @@ import { loginStore } from '../../stores/login.store';
 import { User } from '@supabase/supabase-js';
 import { getIncomingSeances } from '../../services/seances.service';
 import { getDateString } from '../../utils/shared/date';
+
+import './styles.scss';
 
 export default function Seances() {
   const [seances, setSeances] = useState<any[] | null>(null);
@@ -40,24 +41,49 @@ export default function Seances() {
             <h1>Séances</h1>
           </PageHeaderIllu>
           <div className='content animate-in d-flex flex-column flex-align-start w-100 flex-gap-s'>
-            {seances?.map((seance) => {
+            {seances?.map((seance, index) => {
+              const isToday =
+                new Date(seance.date_programmation).setHours(0, 0, 0, 0) ===
+                new Date().setHours(0, 0, 0, 0);
               return (
-                <Link
-                  className='w-100 color-black'
-                  to={`seance/${seance.id}`}
-                  key={seance.id}
-                >
-                  <Bento className='d-flex flex-justify-start flex-align-center flex-gap'>
-                    <Avatar chain={seance.libelle} />
-                    <div className='d-flex flex-column'>
-                      {seance.libelle}
-                      <i style={{ color: '#666' }}>
-                        {getDateString(new Date(seance.date_programmation))}
-                      </i>
-                    </div>
-                    <i className='iconoir-nav-arrow-right m-left-auto'></i>
-                  </Bento>
-                </Link>
+                <>
+                  {isToday && index == 0 && (
+                    <span className='is-today-label' style={{ color: '#888' }}>Séance(s) du jour</span>
+                  )}
+                  <Link
+                    className={`w-100 ${
+                      isToday
+                        ? 'relative bg-primary-r-gradient-shadow border-primary-r border-radius seance-today'
+                        : 'color-black'
+                    }`}
+                    to={`seance/${seance.id}`}
+                    key={seance.id}
+                  >
+                    <Bento
+                      className={`d-flex flex-justify-start flex-align-center flex-gap ${
+                        isToday ? 'bg-primary-r-gradient' : ''
+                      }`}
+                    >
+                      <Avatar
+                        chain={seance.libelle}
+                        couleur={seance.couleur || null}
+                      />
+                      <div className='d-flex flex-column'>
+                        {seance.libelle}
+                        <i
+                          style={{
+                            ...(isToday
+                              ? { color: '#ddd' }
+                              : { color: '#888' }),
+                          }}
+                        >
+                          {getDateString(new Date(seance.date_programmation))}
+                        </i>
+                      </div>
+                      <i className='iconoir-nav-arrow-right m-left-auto'></i>
+                    </Bento>
+                  </Link>
+                </>
               );
             })}
           </div>
